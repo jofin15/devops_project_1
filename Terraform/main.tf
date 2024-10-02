7e7d6de862e30afe90aa8511d83293ca38742f78
jofin15 <70406065+jofin15@users.noreply.github.com>
2024-10-03 00:19:34 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,15 +1,18 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/Ubuntu-*-20.04-amd64-server-*"]
-  }
+  owners = ["099720109477"]  # Ubuntu account ID
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-20.04-amd64-server-*"]
   }
+
+  # Uncomment this filter if needed later
+  # filter {
+  #   name   = "virtualization-type"
+  #   values = ["hvm"]
+  # }
 }
 
 resource "aws_instance" "dev_machine" {
